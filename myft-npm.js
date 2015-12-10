bb0bf37b2016d5178a7d112be5b58c94a40b1ecd
Laurie Boyes <laurie.boyes@ft.com>
2015-12-10 11:46:27 +0000

    temporarily bypass myft-api cdn

--- myft-npm.js
+++ myft-npm.js
@@ -3,7 +3,8 @@
 var MyFTApi = require('./src/myft-api.js');
 
 module.exports = new MyFTApi({
-	apiRoot: process.env.MYFT_API_URL,
+	//apiRoot: process.env.MYFT_API_URL,
+	apiRoot: 'https://ft-next-myft-api.herokuapp.com/v2/',
 	headers: {
 		'X-API-KEY': process.env.USER_PREFS_API_KEY || process.env.MYFT_API_KEY
 	}
