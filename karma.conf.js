079b434a17d496a0d4246328ef235beea2b5b367
gyss <gravedian@gmail.com>
2022-04-19 17:29:02 +0100

    removed Chrome polyfill since have updated its version through Puppeteer

--- karma.conf.js
+++ karma.conf.js
@@ -9,8 +9,6 @@ module.exports = function (karma) {
 	karma.set({
 		frameworks: [ 'mocha', 'chai', 'browserify' ],
 		files: [
-			// CircleCI's Chrome is version 43 at time of writing (no Object.assign support; latest is 47)
-			'https://cdn.polyfill.io/v2/polyfill.min.js?callback=ftNextInit&features=default&ua=chrome/43',
 			'test/browser/**/*.js'
 		],
 		preprocessors: {
