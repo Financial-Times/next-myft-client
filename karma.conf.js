4ef0b3ef4f38fd4aa3b966b24eeaea93e657f39b
gyss <gravedian@gmail.com>
2022-04-21 14:12:38 +0100

    Restore upgrade to node16 that was reverted by this commit 085ebfe0e7e5f93ed2c244f05848b616cded0c88

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const puppeteer = require('puppeteer');
+
+// Use Chrome headless
+process.env.CHROME_BIN = puppeteer.executablePath();
+
 module.exports = function (karma) {
 	karma.set({
 		frameworks: [ 'mocha', 'chai', 'browserify' ],
 		files: [
-			// CircleCI's Chrome is version 43 at time of writing (no Object.assign support; latest is 47)
-			'https://cdn.polyfill.io/v2/polyfill.min.js?callback=ftNextInit&features=default&ua=chrome/43',
 			'test/browser/**/*.js'
 		],
 		preprocessors: {
