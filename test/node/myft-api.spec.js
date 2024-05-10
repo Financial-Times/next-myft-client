34ea97c8c1e21d0026c2cf50e6add3e32ec5894c
Asuka Ochi <asuka.ochi@ft.com>
2024-05-10 12:27:24 +0100

    Re-organize existing tests before adding more tests

--- test/node/myft-api.spec.js
+++ test/node/myft-api.spec.js
@@ -6,97 +6,103 @@ fetchMock.get('*', []);
 
 describe('myFT node API', () => {
 
-	describe('default environment', () => {
+	let MyFtApi = require('../../src/myft-api');
+	let myFtApi = new MyFtApi({ apiRoot: 'https://test-api-route.com/' });
+	const userId = '00000000-0000-0000-0000-000000000001';
 
-		const MyFtApi = require('../../src/myft-api');
-		const myFtApi = new MyFtApi({
-			apiRoot: 'https://test-api-route.com/',
-			headers: {
-				'X-API-KEY': 'adasd'
-			}
-		});
-
+	describe('Library functions', () => {
 		describe('url personalising', function () {
 			it('should be possible to personalise a url', function () {
-
-				const testUuid = '00000000-0000-0000-0000-000000000000';
-
-				expect(myFtApi.personaliseUrl('/myft', testUuid)).to.equal(`/myft/${testUuid}`);
-				expect(myFtApi.personaliseUrl(`/myft/${testUuid}`, testUuid)).to.equal(`/myft/${testUuid}`);
+				expect(myFtApi.personaliseUrl('/myft', userId)).to.equal(`/myft/${userId}`);
+				expect(myFtApi.personaliseUrl(`/myft/${userId}`, userId)).to.equal(`/myft/${userId}`);
 			});
 		});
 
-
 		describe('identifying personalised URLs', function () {
 			it('should identify between personalised urls and not personalised urls', function () {
-				expect(myFtApi.isPersonalisedUrl('/myft/00000000-0000-0000-0000-000000000000')).to.be.true;
+				expect(myFtApi.isPersonalisedUrl(`/myft/${userId}`)).to.be.true;
 				expect(myFtApi.isPersonalisedUrl('/myft/following/')).to.be.false;
 			});
 		});
 
 		describe('identifying immutable URLs', function () {
 			it('should identify between immutable urls and mutable urls', function () {
-				expect(myFtApi.isImmutableUrl('/myft/00000000-0000-0000-0000-000000000000')).to.be.true;
+				expect(myFtApi.isImmutableUrl(`/myft/${userId}`)).to.be.true;
 				expect(myFtApi.isImmutableUrl('/myft/following/')).to.be.false;
-			});
-		});
-
-		describe('getting a relationship', function () {
-
-			it('should request the API', () => {
-				return myFtApi.getAllRelationship('user', 'userId', 'followed', 'concept').then(() => {
-					expect(fetchMock.lastUrl('*')).to.equal('https://test-api-route.com/user/userId/followed/concept');
-				});
-			});
-
-			it('should accept pagination parameters', () => {
-				return myFtApi.getAllRelationship('user', 'userId', 'followed', 'concept', {
-					page: 2,
-					limit: 10
-				}).then(() => {
-					expect(fetchMock.lastUrl('*')).to.equal('https://test-api-route.com/user/userId/followed/concept?page=2&limit=10');
-				});
-			});
-
-			it('should not pass a flag to bypass maintenance mode', () => {
-				return myFtApi.getAllRelationship('user', 'userId', 'followed', 'concept').then(() => {
-					expect(fetchMock.lastOptions('*').headers['ft-bypass-myft-maintenance-mode']).to.not.be.true;
-				});
 			});
 		});
 	});
 
-	describe('when BYPASS_MYFT_MAINTENANCE_MODE flag is set', () => {
-
-		let MyFtApi;
-		let myFtApi;
-
-		let previousBypassMaintenanceMode;
-
-		beforeEach(function () {
-			previousBypassMaintenanceMode = process.env.BYPASS_MYFT_MAINTENANCE_MODE;
-			process.env.BYPASS_MYFT_MAINTENANCE_MODE = true;
-
-			delete require.cache[require.resolve('../../src/myft-api')];
-			MyFtApi = require('../../src/myft-api');
-			myFtApi = new MyFtApi({
-				apiRoot: 'https://test-api-route.com/',
-				headers: {
-					'X-API-KEY': 'adasd'
-				}
+	describe('getAllRelationship', function () {
+		it('should request the API', () => {
+			return myFtApi.getAllRelationship('user', userId, 'followed', 'concept').then(() => {
+				expect(fetchMock.lastUrl('*')).to.equal(`https://test-api-route.com/user/${userId}/followed/concept`);
 			});
 		});
 
-		afterEach(function () {
-			fetchMock.reset();
-			process.env.BYPASS_MYFT_MAINTENANCE_MODE = previousBypassMaintenanceMode;
+		it('should accept pagination parameters', () => {
+			return myFtApi.getAllRelationship('user', userId, 'followed', 'concept', {
+				page: 2,
+				limit: 10
+			}).then(() => {
+				expect(fetchMock.lastUrl('*')).to.equal(`https://test-api-route.com/user/${userId}/followed/concept?page=2&limit=10`);
+			});
 		});
+	});
 
-		it('should pass a flag to bypass maintenance mode', () => {
-			return myFtApi.getAllRelationship('user', 'userId', 'followed', 'concept').then(() => {
-				expect(fetchMock.lastOptions('*').headers['ft-bypass-myft-maintenance-mode']).to.equal('true');
+	describe('Headers', () => {
+
+		const defaultHeaders = { 'Content-Type': 'application/json' };
+		const optsHeaders = { 'x-opts-header': 'x-opts-header-value' };
+
+		it('should not pass a flag to bypass maintenance mode', () => {
+			return myFtApi.getAllRelationship('user', userId, 'followed', 'concept').then(() => {
+				expect(fetchMock.lastOptions('*').headers['ft-bypass-myft-maintenance-mode']).to.not.be.true;
 			});
 		});
 
+		describe('when BYPASS_MYFT_MAINTENANCE_MODE flag is set', () => {
+
+			let previousBypassMaintenanceMode;
+			const bypassMyftMaintenanceHeader = { 'ft-bypass-myft-maintenance-mode': 'true' };
+
+			beforeEach(function () {
+				previousBypassMaintenanceMode = process.env.BYPASS_MYFT_MAINTENANCE_MODE;
+				process.env.BYPASS_MYFT_MAINTENANCE_MODE = true;
+
+				delete require.cache[require.resolve('../../src/myft-api')];
+				MyFtApi = require('../../src/myft-api');
+			});
+
+			afterEach(function () {
+				fetchMock.reset();
+				process.env.BYPASS_MYFT_MAINTENANCE_MODE = previousBypassMaintenanceMode;
+			});
+
+
+			it('should set headers when it is provided', () => {
+				myFtApi = new MyFtApi({
+					apiRoot: 'https://test-api-route.com/',
+					headers: optsHeaders
+				});
+				expect(myFtApi.headers).to.deep.equal({
+					...defaultHeaders,
+					...optsHeaders,
+					...bypassMyftMaintenanceHeader
+				});
+			});
+
+			it('should pass a flag to bypass maintenance mode', () => {
+				myFtApi = new MyFtApi({ apiRoot: 'https://test-api-route.com/' });
+
+				return myFtApi.getAllRelationship('user', userId, 'followed', 'concept').then(() => {
+					expect(fetchMock.lastOptions('*').headers).to.deep.equal({
+						...defaultHeaders,
+						...bypassMyftMaintenanceHeader
+					});
+				});
+			});
+
+		});
 	});
 });
